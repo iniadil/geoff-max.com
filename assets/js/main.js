f8adb9e9eb93978422102baf63169549373060d4
iniadil <35065551+iniadil@users.noreply.github.com>
2020-07-08 17:38:45 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.slick-headsm').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: null,
+        nextArrow: null,
+    })
+
+    $('.slick').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    })
+})
